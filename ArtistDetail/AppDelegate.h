b8cc09c0b7c8afc98d398d5eac0891f7a7c4ef35
JakeyXing <xingjiehai@gmail.com>
2015-01-05 10:01:58 +0800

    Initial Commit

---
+++ ArtistDetail/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ArtistDetail
+//
+//  Created by xingJH on 15-1-5.
+//  Copyright (c) 2015年 Janlent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ArtistDetail/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArtistDetail
+//
+//  Created by xingJH on 15-1-5.
+//  Copyright (c) 2015年 Janlent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
